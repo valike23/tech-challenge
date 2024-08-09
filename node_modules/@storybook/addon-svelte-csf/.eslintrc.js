73829e6c6f48befe17a0e66f75043a04babe8ce3
Sean Knowles <s@seanknowles.dev>
2024-08-09 02:22:11 +0100

    feat: initial commit setting up starter - minor tweeks

---
+++ node_modules/@storybook/addon-svelte-csf/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:storybook/recommended'],
+  rules: {
+    '@typescript-eslint/dot-notation': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/return-await': 'off',
+  },
+  overrides: [],
+};
